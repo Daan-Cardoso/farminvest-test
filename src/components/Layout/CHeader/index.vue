8967fbdbaab352a6a4db13a979c117055e40fa98
daan-cardoso <danilo.cardosolima@gmail.com>
2024-06-20 09:47:20 -0300

    wip: create components - CHeader and initial scss files

---
+++ src/components/Layout/CHeader/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="c-header">
+    <figure class="c-header__logo container">
+      <img class="c-header__logo__image" src="/src/assets/images/logo.png" alt="Logo Covidômetro" />
+    </figure>
+  </header>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.c-header {
+  align-items: center;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  display: flex;
+  height: 70px;
+  justify-content: center;
+  width: 100%;
+
+  &__logo {
+    align-items: center;
+    display: flex;
+
+    &__image {
+      object-fit: cover;
+    }
+  }
+}
+</style>
