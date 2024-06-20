c4668bca6b97dce7e7604894dc0effd91a2abf0f
daan-cardoso <danilo.cardosolima@gmail.com>
2024-06-20 10:56:43 -0300

    wip: create-components - CCard

---
+++ src/components/UI/CCard/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="c-card">
+    <header class="c-card__header" v-if="$slots.header">
+      <slot name="header" />
+    </header>
+
+    <div class="c-card__content" v-if="$slots.default">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss">
+.c-card {
+  width: 100%;
+  border-radius: 6px;
+  border: 2px solid #f8f7f7;
+  padding: 30px;
+}
+</style>
