85ad77d603dbafd5c5410f0c7c4688685ff16d92
daan-cardoso <danilo.cardosolima@gmail.com>
2024-06-20 09:10:45 -0300

    chore: add dev package unplugin-index-extenstion/vite

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import IndexExt from 'unplugin-index-extension/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    IndexExt(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "/src/assets/scss/main.scss";`
+      },
+    },
+  },
 })
