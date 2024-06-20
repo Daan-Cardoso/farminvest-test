e531a15fa4b8b23ac5104ea3986db0799042bdc8
daan-cardoso <danilo.cardosolima@gmail.com>
2024-06-20 10:35:29 -0300

    chore: add oh-vue-icon package

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import * as MdIcons from "oh-vue-icons/icons/md";
 
 import App from './App.vue'
 import router from './router'
+import './assets/scss/reset.scss'
+
+addIcons(...Object.values({ ...MdIcons }));
 
 const app = createApp(App)
-
+app.component("v-icon", OhVueIcon);
 app.use(createPinia())
 app.use(router)
 
