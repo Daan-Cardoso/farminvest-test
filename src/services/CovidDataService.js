0fe3d913eb087a3397161de6f20c1af504745c9e
daan-cardoso <danilo.cardosolima@gmail.com>
2024-06-21 10:59:50 -0300

    chore: add package axios-cache-adapter

--- src/services/CovidDataService.js
+++ src/services/CovidDataService.js
@@ -1,10 +1,17 @@
 import axios from "axios";
+import { setupCache } from 'axios-cache-adapter'
+
+const cache = setupCache({
+  maxAge: 15 * 60 * 1000 // 15 minutes
+})
+
 
 const api = axios.create({
   baseURL: "https://covid-api.com/api",
   headers: {
     "Content-Type": "application/json",
-  }
+  },
+  adapter: cache.adapter
 })
 
 export const fetchAllCountries = async (params) => {
