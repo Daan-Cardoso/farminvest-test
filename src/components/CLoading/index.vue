c35255f5082dc1787d4a3936a31b5ae8225edb14
daan-cardoso <danilo.cardosolima@gmail.com>
2024-06-21 14:38:47 -0300

    feat: create components - c-transition and c-loading

---
+++ src/components/CLoading/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <CCard class="c-loading">
+    <v-icon name="md-autorenew" animation="spin" />
+  </CCard>
+</template>
+
+<script>
+export default {
+  name: 'CLoading'
+}
+</script>
+
+<script setup>
+import CCard from '@/components/CCard'
+</script>
---
+++ src/components/CTransition/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <component :is="isGroup ? TransitionGroup : Transition" :name="name">
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'CTransition'
+}
+</script>
+
+<script setup>
+import { Transition, TransitionGroup } from 'vue'
+defineProps({
+  name: { type: String, default: 'slide-fade' },
+  isGroup: { type: Boolean, default: false }
+})
+</script>
+
+<style lang="scss">
+.slide-fade {
+  &-enter-active,
+  &-leave-active {
+    transition: all 0.5s ease;
+  }
+
+  &-enter-from {
+    opacity: 0;
+    transform: translateX(-30px);
+  }
+
+  &-leave-to {
+    opacity: 0;
+    transform: translateX(30px);
+  }
+}
+</style>
